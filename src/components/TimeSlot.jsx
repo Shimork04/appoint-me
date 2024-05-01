a3c4e337ff03ceb18a9137924c8f1b19ef4423c6
Shimork04 <mauryashivampersbiz@gmail.com>
2024-05-01 17:43:35 +0530

    css edit

--- src/components/TimeSlot.jsx
+++ src/components/TimeSlot.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TimeSlot = ({ timeSlots, handleTimeSlotClick }) => {
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{display:"flex",flexDirection:"column"}}>
       <h2>Time Slots</h2>
       {timeSlots.map((timeSlot, index) => (
         <button key={index} onClick={() => handleTimeSlotClick(timeSlot)}>
